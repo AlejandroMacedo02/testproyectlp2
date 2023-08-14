6500def1f95e9727884c01147b1aa59d726fd04b
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-14 12:25:23 -0500

    proyect initT

--- src/main/java/upeu/edu/pe/firsttestlp2/Firsttestlp2Application.java
+++ src/main/java/upeu/edu/pe/firsttestlp2/Firsttestlp2Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Firsttestlp2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Firsttestlp2Application.class, args);
+	
+            
+            
+            SpringApplication.run(Firsttestlp2Application.class, args);
 	}
+        
+      
 
 }
