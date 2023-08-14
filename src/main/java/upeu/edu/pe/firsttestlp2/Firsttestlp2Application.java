573dd3f899f8c5edc797ff36b2f2d04d850c8e8a
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-14 12:06:53 -0500

    proyect init

---
+++ src/main/java/upeu/edu/pe/firsttestlp2/Firsttestlp2Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.firsttestlp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firsttestlp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firsttestlp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/firsttestlp2/Firsttestlp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.firsttestlp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firsttestlp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
